643aa3139f77f8f25e4275aa9e9600125b67f522
LVotto <luizvotto@gmail.com>
2017-10-06 22:22:54 -0300

    Add magnetic wave calculations and tests
    It works!!

--- sandbox.py
+++ sandbox.py
@@ -8,7 +8,7 @@ Author: Luiz Felipe Machado Votto
 """
 
 from abc import ABC, abstractmethod
-from decimal import Decimal
+#from decimal import Decimal
 
 from scipy import misc
 from scipy import special
@@ -22,6 +22,11 @@ AXICON = np.longdouble(np.pi / 180)  # 1 degree
 MAX_IT = 200
 WAVELENGTH = np.longdouble(1064.0E-9)
 WAVE_NUMBER = 2 * np.pi / WAVELENGTH
+STOP = 10/(WAVE_NUMBER * np.sin(AXICON))
+#STOP = 10/(WAVE_NUMBER) # (to see the peak)
+START = 1e-10
+#START = -STOP
+NUM = 500
 
 
 class Field(ABC):
@@ -67,8 +72,9 @@ class CartesianField(Field):
     def abs(self, x_value, y_value, z_value):
         return np.linalg.norm(
             self.evaluate(x=x_value, y=y_value, z=z_value))
-             
+
 def protected_denominator(value, epsilon=np.longdouble(1E-25)):
+    """ Changes a value that's zero to a small number. """
     if value == 0:
         return epsilon
     else:
@@ -95,7 +101,7 @@ def d2_riccati_bessel_j(degree, argument):
     return 1 / (argument) \
             * (degree + pow(degree, 2) - pow(argument, 2)) \
             * special.spherical_jn(degree, argument)
-            
+
 
 def legendre_p(degree, order, argument):
     """ Associated Legendre function of integer order
@@ -130,7 +136,7 @@ def beam_shape_g_exa(degree, axicon=AXICON, bessel=True):
     return (1 + np.cos(axicon)) / (2 * degree * (degree + 1)) \
             * (legendre_tau(degree, 1, np.cos(axicon)) \
                + legendre_pi(degree, 1, np.cos(axicon)))
-    
+
 
 def beam_shape_g(degree, order, axicon=AXICON, mode='TM'):
     """ Computes BSC in terms of degree and order
@@ -168,7 +174,7 @@ def _radial_electric_i_tm(radial, theta, phi, wave_number_k):
                     / (wave_number_k * radial) \
                     * legendre_p(n, 0, np.cos(theta))
         result += increment
-    
+
     increment = EPSILON
     while m <= MAX_IT and increment >= EPSILON:
         n = m
@@ -205,10 +211,12 @@ def radial_electric_i_tm(radial, theta, phi, wave_number_k):
                       * np.exp(1j * m * phi)
             result += increment
         n += 1
-        
+
     return wave_number_k * result
-        
+
 def theta_electric_i_tm(radial, theta, phi, wave_number_k):
+    """ Computes the theta component of inciding electric field in TM mode.
+    """
     result = 0
     n = 1
     increment = EPSILON + 1
@@ -229,6 +237,8 @@ def theta_electric_i_tm(radial, theta, phi, wave_number_k):
     return result / (radial)
 
 def theta_electric_i_te(radial, theta, phi, wave_number_k):
+    """ Computes the theta component of inciding electric field in TE mode.
+    """
     result = 0
     n = 1
     increment = EPSILON + 1
@@ -248,10 +258,12 @@ def theta_electric_i_te(radial, theta, phi, wave_number_k):
             #if abs(result) > 1e-7:
                 #print('Undesirable', result,'at', radial, 'n = ', n)
         n += 1
-        
+
     return result / (radial)
 
 def phi_electric_i_tm(radial, theta, phi, wave_number_k):
+    """ Computes the phi component of inciding electric field in TM mode.
+    """
     result = 0
     n = 1
     increment = EPSILON + 1
@@ -265,14 +277,16 @@ def phi_electric_i_tm(radial, theta, phi, wave_number_k):
                       * plane_wave_coefficient(n, wave_number_k) \
                       * beam_shape_g(n, m, mode='TM') \
                       * d_riccati_bessel[n] \
-                      * legendre_pi(n, abs(m), 0) \
+                      * legendre_pi(n, abs(m), np.cos(theta)) \
                       * np.exp(1j * m * phi)
             result += increment
         n += 1
-        
+
     return 1j * result / (radial)
 
 def phi_electric_i_te(radial, theta, phi, wave_number_k):
+    """ Computes the phi component of inciding electric field in TE mode.
+    """
     result = 0
     n = 1
     m = 0
@@ -290,31 +304,160 @@ def phi_electric_i_te(radial, theta, phi, wave_number_k):
                       * np.exp(1j * m * phi)
             result += increment
         n += 1
-        
+
     return 1j * result / (radial)
 
 def abs_theta_electric_i(radial, theta, phi, wave_number_k):
+    """ Calculates absolute value of inciding theta component """
     retval = theta_electric_i_tm(radial, theta, phi, wave_number_k) \
              + theta_electric_i_te(radial, theta, phi, wave_number_k)
     return abs(retval)
 
 def abs_phi_electric_i(radial, theta, phi, wave_number_k):
+    """ Calculates absolute value of inciding phi component """
     retval = phi_electric_i_tm(radial, theta, phi, wave_number_k) \
              + phi_electric_i_te(radial, theta, phi, wave_number_k)
     return abs(retval)
-import math
-def square_absolute(radial, theta, phi, wave_number_k):
+
+def square_absolute_electric_i(radial, theta, phi, wave_number_k):
+    """ Calculates absolute value of inciding electric wave """
     retval = pow(abs(radial_electric_i_tm(radial, theta, phi, wave_number_k)), 2)
     retval += pow(abs_theta_electric_i(radial, theta, phi, wave_number_k), 2)
     retval += pow(abs_phi_electric_i(radial, theta, phi, wave_number_k), 2)
     return retval
 
+def theta_magnetic_i_tm(radial, theta, phi, wave_number_k):
+    """ Computes the theta component of inciding magnetic field in TM mode.
+    """
+    result = 0
+    n = 1
+    increment = EPSILON + 1
 
-STOP = 10/(WAVE_NUMBER * np.sin(AXICON))
-#STOP = 10/(WAVE_NUMBER) # (to see the peak)
-START = 1e-6
-#START = -STOP
-NUM = 500
+    riccati_bessel_list = _riccati_bessel_j(MAX_IT, wave_number_k * radial)
+    riccati_bessel = riccati_bessel_list[0]
+
+    while n < MAX_IT and abs(increment) >= EPSILON:
+        for m in [-1, 1]:
+            increment = m \
+                      * plane_wave_coefficient(n, wave_number_k) \
+                      * beam_shape_g(n, m, mode='TM') \
+                      * riccati_bessel[n] \
+                      * legendre_pi(n, abs(m), np.cos(theta)) \
+                      * np.exp(1j * m * phi)
+            result += increment
+        n += 1
+
+    return -result / (radial)
+
+def phi_magnetic_i_tm(radial, theta, phi, wave_number_k):
+    """ Computes the phi component of inciding magnetic field in TM mode.
+    """
+    result = 0
+    n = 1
+    increment = EPSILON + 1
+
+    riccati_bessel_list = _riccati_bessel_j(MAX_IT, wave_number_k * radial)
+    riccati_bessel = riccati_bessel_list[0]
+
+    while n < MAX_IT and abs(increment) >= EPSILON:
+        for m in [-1, 1]:
+            increment = plane_wave_coefficient(n, wave_number_k) \
+                      * beam_shape_g(n, m, mode='TM') \
+                      * riccati_bessel[n] \
+                      * legendre_tau(n, abs(m), np.cos(theta)) \
+                      * np.exp(1j * m * phi)
+            result += increment
+        n += 1
+
+    return -1j * result / (radial)
+
+def radial_magnetic_i_te(radial, theta, phi, wave_number_k):
+    """ Computes the radial component of inciding magnetic field in TE mode.
+    """
+    result = 0
+    n = 1
+    increment = EPSILON + 1
+
+    riccati_bessel_list = _riccati_bessel_j(MAX_IT, wave_number_k * radial)
+    riccati_bessel = riccati_bessel_list[0]
+
+    while n < MAX_IT and abs(increment) >= EPSILON:
+        for m in [-1, 1]:
+            increment = plane_wave_coefficient(n, wave_number_k) \
+                      * beam_shape_g(n, m, mode='TE') \
+                      * (d2_riccati_bessel_j(n, wave_number_k * radial) \
+                         + riccati_bessel[n]) \
+                      * legendre_p(n, abs(m), np.cos(theta)) \
+                      * np.exp(1j * m * phi)
+            result += increment
+        n += 1
+
+    return wave_number_k * result
+
+def theta_magnetic_i_te(radial, theta, phi, wave_number_k):
+    """ Computes the theta component of inciding magnetic field in TE mode.
+    """
+    result = 0
+    n = 1
+    increment = EPSILON + 1
+
+    riccati_bessel_list = _riccati_bessel_j(MAX_IT, wave_number_k * radial)
+    d_riccati_bessel = riccati_bessel_list[1]
+
+    while n < MAX_IT and abs(increment) >= EPSILON:
+        for m in [-1, 1]:
+            increment = plane_wave_coefficient(n, wave_number_k) \
+                      * beam_shape_g(n, m, mode='TE') \
+                      * d_riccati_bessel[n] \
+                      * legendre_tau(n, abs(m), np.cos(theta)) \
+                      * np.exp(1j * m * phi)
+            result += increment
+        n += 1
+
+    return result / (radial)
+
+def phi_magnetic_i_te(radial, theta, phi, wave_number_k):
+    """ Computes the phi component of inciding magnetic field in TE mode.
+    """
+    result = 0
+    n = 1
+    increment = EPSILON + 1
+
+    riccati_bessel_list = _riccati_bessel_j(MAX_IT, wave_number_k * radial)
+    d_riccati_bessel = riccati_bessel_list[1]
+
+    while n < MAX_IT and abs(increment) >= EPSILON:
+        for m in [-1, 1]:
+            increment = m \
+                      * plane_wave_coefficient(n, wave_number_k) \
+                      * beam_shape_g(n, m, mode='TE') \
+                      * d_riccati_bessel[n] \
+                      * legendre_pi(n, abs(m), np.cos(theta)) \
+                      * np.exp(1j * m * phi)
+            result += increment
+        n += 1
+
+    return 1j * result / (radial)
+
+def abs_theta_magnetic_i(radial, theta, phi, wave_number_k):
+    """ Calculates absolute value of inciding theta component """
+    retval = theta_magnetic_i_tm(radial, theta, phi, wave_number_k) \
+             + theta_magnetic_i_te(radial, theta, phi, wave_number_k)
+    return abs(retval)
+
+def abs_phi_magnetic_i(radial, theta, phi, wave_number_k):
+    """ Calculates absolute value of inciding phi component """
+    retval = phi_magnetic_i_tm(radial, theta, phi, wave_number_k) \
+             + phi_magnetic_i_te(radial, theta, phi, wave_number_k)
+    return abs(retval)
+
+def square_absolute_magnetic_i(radial, theta, phi, wave_number_k):
+    """ Calculates absolute value of inciding magnetic wave """
+    retval = pow(abs(radial_magnetic_i_te(radial, theta, phi, wave_number_k)), 2)
+    retval += pow(abs_theta_magnetic_i(radial, theta, phi, wave_number_k), 2)
+    retval += pow(abs_phi_magnetic_i(radial, theta, phi, wave_number_k), 2)
+    return retval
+
 
 def normalize_list(lst):
     try:
@@ -325,20 +468,20 @@ def normalize_list(lst):
         for sublist in lst:
             normalize_list(sublist)
     if not is_parent:
-        for i in range(0,len(lst)):
+        for i in range(0, len(lst)):
             lst[i] /= maxlst
 
 def do_some_plotting(function, *args, start=START, stop=STOP / 1E-6, num=NUM, normalize=False):
-        t = np.linspace(start, stop, num=num)
-        s=[]
-        for j in t:
-            s.append(function(j * 1E-6, *args))
-        print('DONE:', function.__name__)
-        if normalize:
-            normalize_list(s)
-        maxs = max(s)
-        print('PEAK = ', maxs, ' at ', t[s.index(maxs)])
-        plt.plot(t, s)
+    t = np.linspace(start, stop, num=num)
+    s = []
+    for j in t:
+        s.append(function(j * 1E-6, * args))
+    print('DONE:', function.__name__)
+    if normalize:
+        normalize_list(s)
+    maxs = max(s)
+    print('PEAK = ', maxs, ' at ', t[s.index(maxs)])
+    plt.plot(t, s)
 
 def radial_electric_tm_increment(max_it,
                                  radial,
@@ -357,7 +500,7 @@ def radial_electric_tm_increment(max_it,
                       * legendre_p(n, abs(m), np.cos(theta)) \
                       * np.exp(1j * m * phi)
             result += increment
-    return abs(result)
+    return abs(wave_number_k * result)
 
 def bessel_0(argument, scale):
     return pow(special.j0(scale * argument), 2)
@@ -368,16 +511,10 @@ def get_max_it(x_max, wave_number_k=WAVE_NUMBER):
                    * pow(wave_number_k * x_max, 1/3)) + 2)
 
 def difference_x(radial):
-    return square_absolute(radial, np.pi/2, 0, WAVE_NUMBER) \
+    return square_absolute_electric_i(radial, np.pi/2, 0, WAVE_NUMBER) \
            - bessel_0(radial, WAVE_NUMBER * np.sin(AXICON))
 
 MAX_IT = get_max_it(STOP)
-#MAX_IT = 100
-print('MAX_IT = ', MAX_IT)
-#for MAX_IT in range(2,20):
-do_some_plotting(bessel_0, WAVE_NUMBER*np.sin(AXICON))
-do_some_plotting(square_absolute, np.pi/2, 0, WAVE_NUMBER)
-plt.show()
 
 
 def plot_increment(x):
@@ -385,18 +522,42 @@ def plot_increment(x):
     s = []
     for n in rng:
         s.append(radial_electric_tm_increment(n, x))
-    for n in range(rng[-1],1000):
+    plt.plot(n, s[-1], 'ro')
+    for n in range(rng[-1], 1000):
         s.append(s[-1])
     plt.plot(range(0,1000), s)
 
+def test_e_field_vs_bessel():
+    #MAX_IT = 100
+    print('MAX_IT = ', MAX_IT)
+    #for MAX_IT in range(2,20):
+    do_some_plotting(bessel_0, WAVE_NUMBER*np.sin(AXICON))
+    do_some_plotting(square_absolute_electric_i, np.pi/2, 0, WAVE_NUMBER)
+    plt.show()
 
-plot_increment(np.longdouble(3.93658110144e-07))
-plot_increment(np.longdouble(3.93658110144e-06))
-plot_increment(2 * np.longdouble(3.93658110144e-06))
-plot_increment(4 * np.longdouble(3.93658110144e-06))
-plot_increment(np.longdouble(np.longdouble(3.93658110144e-05)))
+def test_radial_convergence(sample=10, maxx=STOP):
+    t = np.linspace(0, maxx, sample)
+    for x in t:
+        plot_increment(x)
+        plt.title(x)
+        plt.show()
+        
+def test_h_field_vs_bessel():
+    #MAX_IT = 100
+    print('MAX_IT = ', MAX_IT)
+    #for MAX_IT in range(2,20):
+    do_some_plotting(bessel_0, WAVE_NUMBER*np.sin(AXICON))
+    do_some_plotting(square_absolute_magnetic_i, np.pi/2, 0, WAVE_NUMBER)
+    plt.show()
+
+test_h_field_vs_bessel()
+#test_e_field_vs_bessel()
+#test_radial_convergence()
+
 
 """
+plot_increment(10/(WAVE_NUMBER * np.sin(AXICON)))
+
 rng = np.linspace(0, STOP, 10000)
 s1 = []
 for x in rng:
@@ -418,5 +579,3 @@ plt.plot(rng, s1)
 plt.show()
 """
 
-
-    
